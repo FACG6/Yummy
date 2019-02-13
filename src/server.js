cabdf319cc185455abaf26a8c0e52d9d4a36b251
AbdallahAmmar96 <42452940+AbdallahAmmar96@users.noreply.github.com>
2019-02-13 16:54:23 +0200

    israaaa
    i swear im israa

--- src/server.js
+++ src/server.js
@@ -1,9 +1,8 @@
 const http = require('http');
-const port = 6262;
+const port = process.env.PORT || 6262;
 const router = require('./router')
 
 const server = http.createServer(router);
-
 server.listen(port, () => {
     console.log(`server is running at localhost${port}`);
 })
